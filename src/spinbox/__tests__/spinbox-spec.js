d57b4b9884a41af401b067a4dfff62c7475652c3
KimCoding <jeokrang@hanmail.net>
2017-05-05 15:40:42 +0900

    write the third example

--- src/spinbox/__tests__/spinbox-spec.js
+++ src/spinbox/__tests__/spinbox-spec.js
@@ -9,11 +9,174 @@ import Spinbox from '../spinbox';
  * - 기본값 200
  * - 버튼 클릭하여 값 1씩 증가
  * - 버튼 클릭하여 값 1씩 감소
+ * - 숫자 입력
  */
-
 describe('spinbox.js >', () => {
-    it('스핀박스를 렌더링 할 수 있다.', () => {
-        const spinbox = mount(<Spinbox />);
-        expect(spinbox).to.exist;
+
+    const defaultValue = 200;
+
+    let spinbox;
+    let input;
+
+    function getCurrentValueFromInputNode() {
+        return Number(input.getDOMNode().value);
+    }
+
+    beforeEach(() => {
+        // 픽스처 생성
+        spinbox = mount(<Spinbox />);
+        input = spinbox.find('[data-role="value"]');
+    });
+
+    afterEach(() => {
+        // 픽스처 정리
+    });
+
+    describe('', () => {
+        it('기본값이 200인 스핀박스를 렌더링 할 수 있다.', () => {
+            // given
+            // when
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(defaultValue);
+        });
+    });
+
+    describe('증가 >', () => {
+
+        let increaseBtn;
+
+        beforeEach(() => {
+            increaseBtn = spinbox.find('[data-role="increase"]');
+        });
+
+        it('증가 버튼을 클릭하여 값을 1 증가시킬 수 있다.', () => {
+            // given
+            // when
+            increaseBtn.simulate('click');
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(defaultValue + 1);
+        });
+
+        it('증가 버튼을 두 번 클릭하여 값을 2 증가시킬 수 있다.', () => {
+            // given
+            // when
+            increaseBtn.simulate('click');
+            increaseBtn.simulate('click');
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(defaultValue + 2);
+        });
+
+        it('증가 버튼을 세 번 클릭하여 값을 3 증가시킬 수 있다.', () => {
+            // given
+            // when
+            increaseBtn.simulate('click');
+            increaseBtn.simulate('click');
+            increaseBtn.simulate('click');
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(defaultValue + 3);
+        });
+
+        it('입력 폼에 0을 입력한 후 증가 버튼을 클릭하여 값을 증가시킬 수 있다.', () => {
+            // given
+            const inputValue = 0;
+
+            input.simulate('change', {
+                target: { value: String(inputValue) }
+            });
+
+            // when
+            increaseBtn.simulate('click');
+            increaseBtn.simulate('click');
+            increaseBtn.simulate('click');
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(inputValue + 3);
+        });
+    });
+
+    describe('감소 >', () => {
+        let decreaseBtn;
+
+        beforeEach(() => {
+            decreaseBtn = spinbox.find('[data-role="decrease"]');
+        });
+
+        it('감소 버튼을 클릭하여 값을 1 감소시킬 수 있다.', () => {
+            // given
+            // when
+            decreaseBtn.simulate('click');
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(defaultValue - 1);
+        });
+
+        it('감소 버튼을 세 번 클릭하여 값을 3 감소시킬 수 있다.', () => {
+            // given
+            // when
+            decreaseBtn.simulate('click');
+            decreaseBtn.simulate('click');
+            decreaseBtn.simulate('click');
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(defaultValue - 3);
+        });
+
+        it('입력 폼에 0을 입력한 후 증가 버튼을 클릭하여 값을 증가시킬 수 있다.', () => {
+            // given
+            const inputValue = 0;
+
+            input.simulate('change', {
+                target: { value: String(inputValue) }
+            });
+
+            // when
+            decreaseBtn.simulate('click');
+            decreaseBtn.simulate('click');
+            decreaseBtn.simulate('click');
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(inputValue - 3);
+        });
+    });
+
+    describe('입력 >', () => {
+        it('입력 폼에 0을 입력할 수 있다.', () => {
+            // given
+            const inputValue = 0;
+
+            // when
+            input.simulate('change', {
+                target: { value: String(inputValue) }
+            });
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(inputValue);
+        });
+
+        it('입력 폼에 999를 입력할 수 있다.', () => {
+            // given
+            const inputValue = 999;
+
+            // when
+            input.simulate('change', {
+                target: { value: String(inputValue) }
+            });
+
+            // then
+            const actualValue = getCurrentValueFromInputNode();
+            expect(actualValue).to.be.eql(inputValue);
+        });
     });
 });
--- src/spinbox/spinbox.js
+++ src/spinbox/spinbox.js
@@ -1,14 +1,46 @@
 import React, {Component} from 'react';
 
 class Spinbox extends Component {
+    componentWillMount() {
+        this.state = {
+            value: 200
+        };
+        this.handleClickIncrease = this.handleClickIncrease.bind(this);
+        this.handleClickDecrease = this.handleClickDecrease.bind(this);
+        this.handleChangeInput = this.handleChangeInput.bind(this);
+    }
+
     render() {
         return (
             <div>
-                <input type="text" />
-                <button>▲</button>
-                <button>▼</button>
+                <input data-role="value"
+                       type="text"
+                       value={ this.state.value }
+                       onChange={ this.handleChangeInput } />
+                <button data-role="increase"
+                        onClick={ this.handleClickIncrease }>▲</button>
+                <button data-role="decrease"
+                        onClick={ this.handleClickDecrease }>▼</button>
             </div>
         );
+    }
+
+    handleChangeInput(e) {
+        this.setState({
+            value: Number(e.target.value)
+        });
+    }
+
+    handleClickIncrease() {
+        this.setState({
+            value: this.state.value + 1
+        });
+    }
+
+    handleClickDecrease() {
+        this.setState({
+            value: this.state.value - 1
+        });
     }
 }
 
