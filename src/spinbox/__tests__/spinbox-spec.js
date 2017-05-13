44b1519f79a73471a5b00dfbdfa847920693aca7
KimCoding <jeokrang@hanmail.net>
2017-05-13 14:27:39 +0900

    Initial Commit

--- src/spinbox/__tests__/spinbox-spec.js
+++ src/spinbox/__tests__/spinbox-spec.js
@@ -1,15 +1,132 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 
 import Spinbox from '../spinbox';
 
 describe('spinbox.js >', () => {
-    it('스핀박스 컴포넌트를 생성할 수 있다.', () => {
-        // given
-        // when
-        const spinbox = mount(<Spinbox />);
+  let spinbox;
 
-        // then
-        expect(spinbox).toBeDefined();
+  beforeEach(() => {
+    spinbox = mount(<Spinbox />);
+  });
+
+  function getValueFromInputNode(input) {
+    const inputNode = input.getDOMNode();
+    return Number(inputNode.value);
+  }
+
+  describe('기본값 >', () => {
+    it('스핀박스를 생성하면 기본값이 200이어야 한다.', () => {
+      // given
+      // when
+      // then
+      const input = spinbox
+        .find('input')
+        .getDOMNode();
+
+      const actualValue = Number(input.value);
+      const defaultValue = 200;
+
+      expect(actualValue).toEqual(defaultValue);
     });
+  });
+
+  describe('값 입력 >', () => {
+    let input;
+
+    beforeEach(() => {
+      input = spinbox.find('input');
+    });
+
+    it('입력 폼에 999를 입력할 수 있다.', () => {
+      // given
+      // when
+      input.simulate('change', {
+        target: {
+          value: '999'
+        }
+      });
+
+      // then
+      const actualValue = getValueFromInputNode(input);
+      expect(actualValue).toEqual(999);
+    });
+
+    it('입력 폼에 0을 입력할 수 있다.', () => {
+      // given
+      // when
+      input.simulate('change', {
+        target: {
+          value: '0'
+        }
+      });
+
+      // then
+      const actualValue = getValueFromInputNode(input);
+      expect(actualValue).toEqual(0);
+    });
+  });
+
+  describe('값 증가 >', () => {
+    const defaultValue = 200;
+    let incrementBtn;
+
+    beforeEach(() => {
+      incrementBtn = spinbox.find('[data-name="increment"]');
+    });
+
+    it('증가 버튼을 클릭하여 값을 1 증가시킬 수 있다.', () => {
+      // given
+      // when
+      incrementBtn.simulate('click');
+
+      // then
+      const actualValue = getValueFromInputNode(spinbox.find('input'));
+      expect(actualValue).toEqual(defaultValue + 1);
+    });
+
+    it('증가 버튼을 세 번 클릭하여 값을 3 증가시킬 수 있다.', () => {
+      // given
+      // when
+      incrementBtn.simulate('click');
+      incrementBtn.simulate('click');
+      incrementBtn.simulate('click');
+
+      // then
+      const actualValue = getValueFromInputNode(spinbox.find('input'));
+      expect(actualValue).toEqual(defaultValue + 3);
+    });
+  });
+
+
+  describe('값 감소 >', () => {
+    const defaultValue = 200;
+    let decrementBtn;
+
+    beforeEach(() => {
+      decrementBtn = spinbox.find('[data-name="decrement"]');
+    });
+
+    it('감소 버튼을 클릭하여 값을 1 감소시킬 수 있다.', () => {
+      // given
+      // when
+      decrementBtn.simulate('click');
+
+      // then
+      const actualValue = getValueFromInputNode(spinbox.find('input'));
+      expect(actualValue).toEqual(defaultValue - 1);
+    });
+
+    it('감소 버튼을 세 번 클릭하여 값을 3 감소시킬 수 있다.', () => {
+      // given
+      // when
+      decrementBtn.simulate('click');
+      decrementBtn.simulate('click');
+      decrementBtn.simulate('click');
+
+      // then
+      const actualValue = getValueFromInputNode(spinbox.find('input'));
+      expect(actualValue).toEqual(defaultValue - 3);
+    });
+  });
 });
--- src/spinbox/spinbox.jsx
+++ src/spinbox/spinbox.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
-const Spinbox = () => (
-    <div>
-        <input type="text" />
-        <button>▲</button>
-        <button>▼</button>
-    </div>
-);
+class Spinbox extends React.Component {
+  componentWillMount() {
+    this.state = {
+      value: 200
+    };
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.handleClickIncrement = this.handleClickIncrement.bind(this);
+    this.handleClickDecrement = this.handleClickDecrement.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text"
+               value={ this.state.value }
+               onChange={ this.handleChangeInput } />
+        <button data-name="increment"
+                onClick={ this.handleClickIncrement }>▲</button>
+        <button data-name="decrement"
+                onClick={ this.handleClickDecrement }>▼</button>
+      </div>
+    );
+  }
+
+  handleClickDecrement() {
+    this.setState({
+      value: this.state.value - 1
+    });
+  }
+
+  handleClickIncrement() {
+    this.setState({
+      value: this.state.value + 1
+    });
+  }
+
+  handleChangeInput(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+}
 
 Spinbox.defaultProps = {};
 Spinbox.propTypes = {};
